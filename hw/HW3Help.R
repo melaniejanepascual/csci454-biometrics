1efe3e98e93a301b3362753df0413d831ab5758c
MJ <melaniejane.pascual@gmail.com>
2017-11-28 14:12:55 -0500

    clean up

---
+++ hw/HW3Help.R
@@ -0,0 +1,17 @@
+# Homework 3 help
+library(png)
+readPNG("") => {0,1} # range from 0 to 1
+
+## dark image example
+{0.1, 0.3}
+
+## Normalizing the pixel values
+pt - minValue
+max - min()
+
+dim (img) # dimensions should be square, 60x60 or 120x120
+
+img[c(1:20, c(1:20))] gives top left region of a 60 x 60 photo
+
+a = matrix(c(1:36), nrow = 6, ncol = 6)
+a[c(1:3), c(1:3)] ## gets the top left
